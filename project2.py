ac9eb4faba4e02399368bdfd4a782547e80b4f1d
vivaan-colab <vidya89in@gmail.com>
2026-01-03 20:32:40 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,10 @@
+ch=input("enter a character")
+alphabet=["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
+if ch is alphabet:
+    print("this character is an alphabet")
+elif ch is float:
+    print("this character is a float")
+else:
+    print("sorry but there is an error")
+    
+
